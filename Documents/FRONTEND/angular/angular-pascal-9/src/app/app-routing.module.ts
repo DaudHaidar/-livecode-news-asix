029f96d3c4759411e1279e1566475f832db3fea5
abdillahdaudhaidar <abdillahdaudhaidar@gmail.com>
2022-08-31 11:58:39 +0700

    eager routing

--- Documents/FRONTEND/angular/angular-pascal-9/src/app/app-routing.module.ts
+++ Documents/FRONTEND/angular/angular-pascal-9/src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { ArticleComponent } from './pages/article/article.component';
+import { AboutComponent } from './pages/master-home/about/about.component';
 import { HomeComponent } from './pages/master-home/home/home.component';
 import { ChildAComponent } from './sample-rout/component-a/child-a/child-a.component';
 import { ChildBComponent } from './sample-rout/component-a/child-b/child-b.component';
@@ -12,23 +14,23 @@ import { NotFoundComponent } from './shared/components/not-found/not-found.compo
 const routes: Routes = [
     {
         path:'home', component: HomeComponent
-    },
-    {
-    path: 'component-a', component: ComponentAComponent, children:[
-        {
-        path: 'child-a', component: ChildAComponent
-        },
-        {
-            path: 'child-b/:id', component: ChildBComponent
-        }
-    ]
-},{
-    path: 'component-b', component: ComponentBComponent
-},{
+    },{
+
     path: '', redirectTo: 'home' ,pathMatch: 'full'
-},{
+
+    },{
+
+    path: 'tentang-kami', component: AboutComponent
+        
+    },{
+
+    path: 'article', component: ArticleComponent
+            
+    },{
+
     path: '**', component: NotFoundComponent
-}]; 
+
+    }]; 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
