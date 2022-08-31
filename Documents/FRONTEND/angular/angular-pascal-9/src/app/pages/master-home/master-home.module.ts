cbd1df8aca10d504fbeef9557974da0aea09f1a3
abdillahdaudhaidar <abdillahdaudhaidar@gmail.com>
2022-08-31 16:58:08 +0700

    finallivecode

--- Documents/FRONTEND/angular/angular-pascal-9/src/app/pages/master-home/master-home.module.ts
+++ Documents/FRONTEND/angular/angular-pascal-9/src/app/pages/master-home/master-home.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { MasterHomeComponent } from './master-home.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ChildMemberComponent } from './about/child-member/child-member.component';
+import { ChildSuperAppComponent } from './about/child-super-app/child-super-app.component';
+import { ChildCoachingComponent } from './about/child-coaching/child-coaching.component';
 
 
 
@@ -10,7 +13,10 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     MasterHomeComponent,
     AboutComponent,
-    HomeComponent
+    HomeComponent,
+    ChildMemberComponent,
+    ChildSuperAppComponent,
+    ChildCoachingComponent
   ],
   imports: [
     CommonModule
